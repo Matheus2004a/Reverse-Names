968afc7975bca89ef71731b05f3470b7e69fc3e1
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-26 19:22:30 -0300

    Adicionando trecho html no arquivo php do nome invertido

--- Nome-Invertido.php
+++ Nome-Invertido.php
@@ -1,18 +1,30 @@
-<?php
-    $nome = array($_POST['nomeUser1'],$_POST['nomeUser2'],$_POST['nomeUser3'],$_POST['nomeUser4'],$_POST['nomeUser5'],$_POST['nomeUser6'],$_POST['nomeUser7'],$_POST['nomeUser8']);
-    
-    for($i = 0; $i < 8; $i++){
-        echo "$nome[$i] <br>";
-    }
-    echo "<br>";
-
-    // Array anterior na forma invertida
-    $arrayInvertido = array($_POST['nomeUser8'],$_POST['nomeUser7'],$_POST['nomeUser6'],$_POST['nomeUser5'],$_POST['nomeUser4'],$_POST['nomeUser3'],$_POST['nomeUser2'],$_POST['nomeUser1']);
-
-    echo "Os nomes na forma invertida é: <br>";
-    // Imprimindo array anterior na forma invertida
-    for($i = 0; $i < 8; $i++){
-        print_r($arrayInvertido[$i]);
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomes Invertidos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        $nome = array($_POST['nomeUser1'],$_POST['nomeUser2'],$_POST['nomeUser3'],$_POST['nomeUser4'],$_POST['nomeUser5'],$_POST['nomeUser6'],$_POST['nomeUser7'],$_POST['nomeUser8']);
+        
+        for($i = 0; $i < count($nome); $i++){
+            echo "$nome[$i] <br>";
+        }
         echo "<br>";
-    }
-?>
+
+        // Array anterior na forma invertida
+        $arrayInvertido = array($_POST['nomeUser8'],$_POST['nomeUser7'],$_POST['nomeUser6'],$_POST['nomeUser5'],$_POST['nomeUser4'],$_POST['nomeUser3'],$_POST['nomeUser2'],$_POST['nomeUser1']);
+
+        echo "Os nomes na forma invertida é: <br>";
+        // Imprimindo array anterior na forma invertida
+        for($i = 0; $i < count($arrayInvertido); $i++){
+            print_r($arrayInvertido[$i]);
+            echo "<br>";
+        }
+    ?>
+</body>
+</html>
