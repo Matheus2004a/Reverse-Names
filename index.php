50736c98b8da5f41cfd04ed1aef18d2e8606bc4e
Matheus Aurélio <matheusaurelio2004@gmail.com>
2021-10-26 18:08:01 -0300

    Renomeando arquivo de estilo style.css para styles.css

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arrays PHP - Nome Invertido</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
     <div>
-        <form action="Nome-Invertido.php" method="post">
-            <h2>Exibindo lista de nomes invertidos</h2>
+        <form action="Nome-Invertido.php" method="post" id="forms">
+            <h2 id="title-forms">Exibindo lista de nomes invertidos</h2>
             <div class="form-floating mb-3">
                 <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="nomeUser1" required>
                 <label for="floatingInput">Digite um nome:</label>
